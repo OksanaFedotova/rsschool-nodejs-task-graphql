661dcf6dc00f677b50f7a2d076693a37c2b048c9
OksanaFedotova <oxana-fedotova@yandex.ru>
2023-07-22 19:28:37 +0300

    feat: add queries graphql

---
+++ src/routes/graphql/types/posts.ts
@@ -0,0 +1,39 @@
+import {
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
+import { UUIDType } from './uuid.js';
+import { UserType } from './user.js';
+
+export interface PostInput {
+  title: string;
+  content: string;
+  authorId: string;
+}
+
+export const PostType = new GraphQLObjectType({
+  name: 'Post',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+  }),
+});
+export const createPostInputType = new GraphQLInputObjectType({
+  name: 'CreatePostInput',
+  fields: {
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(UUIDType) },
+  },
+});
+
+export const changePostInputType = new GraphQLInputObjectType({
+  name: 'ChangePostInput',
+  fields: {
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+  },
+});
--- src/routes/graphql/types/user.ts
+++ src/routes/graphql/types/user.ts
@@ -1,16 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import {
-  GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt,
-  GraphQLID,
-  GraphQLList,
   GraphQLNonNull,
   GraphQLInputObjectType,
   GraphQLFloat,
+  GraphQLList,
 } from 'graphql';
 import { UUIDType } from './uuid.js';
+import { PostType } from './posts.js';
+import { ProfileType } from './profiles.js';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const UserType = new GraphQLObjectType({
   name: 'UserType',
@@ -19,6 +20,16 @@ export const UserType = new GraphQLObjectType({
     id: { type: UUIDType },
     name: { type: GraphQLString },
     balance: { type: GraphQLFloat },
+    profile: {
+      type: ProfileType as GraphQLObjectType,
+      resolve: async ({ id }: { id: string }) =>
+        await prisma.profile.findUnique({ where: { userId: id } }),
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: async ({ id }: { id: string }) =>
+        prisma.post.findMany({ where: { authorId: id } }),
+    },
   }),
 });
 
